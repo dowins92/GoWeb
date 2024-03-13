6061e4db3a9b4f3557f42818042d4b0570dbd0c5
OSwald Aguilar Dowins <115893639+dowins92@users.noreply.github.com>
2024-03-12 23:18:47 -0400

    Finalizando formulario
    Completando los detalles del formulario de la seccion 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
@@ -15,8 +22,14 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
-
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatRadioModule,
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
